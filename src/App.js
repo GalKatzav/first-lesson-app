fbb07a916e1b773f820820e3e9835747e4d75573
galKatzav <galoosh1591@gmail.com>
2024-04-01 10:24:08 +0300

    app css, counter rfc, bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Counter from "./component/Counter";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,7 +1,33 @@
-import React from 'react'
-
+/*
+import React, { useState } from "react";
 export default function Counter() {
+  const [counter, setCounter] = useState(3);
   return (
-    <div>Counter</div>
-  )
+    <div>
+      <h2>Counter: {counter}</h2>
+    </div>
+  );
+}
+*/
+
+import React, { useState } from "react";
+export default function Counter() {
+  const [counter, setCounter] = useState(3);
+  const onAdd1Click = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div>
+      <h2 className="text-danger">Counter: {counter}</h2>
+      <button onClick={onAdd1Click}>Add 1</button>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        reduce 1
+      </button>
+    </div>
+  );
 }
